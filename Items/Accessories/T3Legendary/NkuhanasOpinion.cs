4f70f2f1041521602950df8caf5847e43179b106
Patrick <95663450+fryguythe777th@users.noreply.github.com>
2024-04-18 11:23:48 -0400

    unlock nkuhanas opinion

--- Items/Accessories/T3Legendary/NkuhanasOpinion.cs
+++ Items/Accessories/T3Legendary/NkuhanasOpinion.cs
@@ -12,11 +12,6 @@ namespace RiskOfTerrain.Items.Accessories.T3Legendary
 {
     public class NkuhanasOpinion : ModAccessory
     {
-        public override bool IsLoadingEnabled(Mod mod)
-        {
-            return false;
-        }
-
         public override void SetStaticDefaults()
         {
             Item.ResearchUnlockCount = 1;

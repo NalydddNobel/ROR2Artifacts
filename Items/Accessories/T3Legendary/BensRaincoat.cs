5e139bdf4aafb36fed19b902b988f02b3e89e6ea
Patrick <95663450+fryguythe777th@users.noreply.github.com>
2024-04-18 11:21:24 -0400

    unlock bens raincoat

--- Items/Accessories/T3Legendary/BensRaincoat.cs
+++ Items/Accessories/T3Legendary/BensRaincoat.cs
@@ -13,11 +13,6 @@ namespace RiskOfTerrain.Items.Accessories.T3Legendary
 {
     public class BensRaincoat : ModAccessory
     {
-        public override bool IsLoadingEnabled(Mod mod)
-        {
-            return false;
-        }
-
         public override void SetStaticDefaults()
         {
             Item.ResearchUnlockCount = 1;
